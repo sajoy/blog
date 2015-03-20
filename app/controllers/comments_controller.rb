76cc030fb7c0c351bdef9bbfbff2d7b9a021a460
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-03-20 11:53:30 -0700

    add comments edit, delete functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,29 +6,38 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comments_params)
+    @comment = @post.comments.new(comment_params)
     if @comment.save
       flash[:notice] = "Thanks for commenting."
       redirect_to post_path(@post)
     else
       flash[:alert] = "So sorry, something went wrong. Try again."
       redirect_to :back
-    else
     end
   end
 
   def edit
     @post = Post.find(params[:post_id])
-
+    @comment = Comment.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:post_id])
-
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Your revised comment was submitted."
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Something went wrong. Please try again."
+      redirect_to :back
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 
 private
