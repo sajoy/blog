b5fa997bc0e60bcbb0a7fe5b3701fe69d2b57ac4
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-04-03 14:25:23 -0700

    add specs testing ajax via paths

--- spec/features/add_comment_page_spec.rb
+++ spec/features/add_comment_page_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe "the new comment process", js: true do
+  it "will not load the new_comment path" do
+    user = FactoryGirl.create(:user)
+    login(user)
+    post = FactoryGirl.create(:post)
+    user.posts.push(post)
+    visit posts_path
+    click_on post.title
+    click_on 'Leave a comment'
+    expect(current_path).== post_path(post)
+  end
+
   it "creates a new comment" do
     user = FactoryGirl.create(:user)
     login(user)
@@ -26,5 +37,20 @@ describe "the new comment process", js: true do
     click_on 'Leave a comment'
     click_on 'Comment'
     expect(page).to have_content 'blank'
+  end
+
+  it "will update the number of comments" do
+    user = FactoryGirl.create(:user)
+    login(user)
+    post = FactoryGirl.create(:post)
+    user.posts.push(post)
+    visit posts_path
+    click_on post.title
+    click_on 'Leave a comment'
+    fill_in 'Content', :with => 'You should think about food, cause food is yummy.'
+    click_on 'Comment'
+    within('div.comments') do
+      expect(page).to have_content post.comments.length
+    end
   end
 end
--- spec/features/update_comment_page_spec.rb
+++ spec/features/update_comment_page_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 describe "the edit post process" do
+
+  it "will not load the new_comment path", js: true do
+    user = FactoryGirl.create(:user)
+    login(user)
+    post = FactoryGirl.create(:post)
+    user.posts.push(post)
+    comment = Comment.create({
+      content: "LOL.",
+      post_id: post.id,
+      user_id: user.id})
+    visit post_path(post)
+    click_on 'edit'
+    expect(current_path).== post_path(post)
+  end
+
   it "updates a comment" do
     user = FactoryGirl.create(:user)
     login(user)
@@ -15,7 +30,9 @@ describe "the edit post process" do
     click_on 'edit'
     fill_in 'Content', :with => 'LMAO!!!'
     click_on 'Comment'
-    expect(page).to have_content 'revision'
+    within('div.comments') do
+      expect(page).to have_content 'LMAO!!!'
+    end
   end
 
   it "will not update if content is empty" do
@@ -46,8 +63,7 @@ describe "the edit post process" do
       user_id: user.id})
     visit posts_path
     click_on post.title
-    click_on 'edit'
-    click_on 'or just toss it in the virtual trash can'
+    click_on 'delete'
     expect(page).to have_no_content comment.content
   end
 
